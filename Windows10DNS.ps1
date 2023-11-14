de6cbefce9b97e5310b189c18c99a7749bb4e623
Damien Fajole <loupegame@gmail.com>
2023-11-14 11:20:08 +0100

    Exit

--- Windows10DNS.ps1
+++ Windows10DNS.ps1
@@ -20,6 +20,7 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     }
     #elevate script and exit current non-elevated runtime
     Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+    exit
 }
 
 $userProfile = $args[0]
